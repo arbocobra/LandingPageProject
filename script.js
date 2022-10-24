94db178d6bf191686f8a405df13c56f179ba624a
arbocobra <63871187+arbocobra@users.noreply.github.com>
2022-10-24 12:57:08 -0400

    Restore original version files
    Vanilla JS + HTML, minimal styling. Following commit will confirm functionality before moving on to next draft of project restoration

---
+++ script.js
@@ -0,0 +1,100 @@
+import { orderedID, orderedTags } from './altTags.js';
+
+window.addEventListener('load', (event) => {
+    displayTags();
+  });
+
+const metUrl = 'https://collectionapi.metmuseum.org/public/collection/v1/objects'
+
+
+const getTags = () => {
+    const tagArray = [];
+    do {
+        const tagNum = Math.floor(Math.random() * orderedID.length);
+        const tag = orderedTags[tagNum];
+        const id = orderedID[tagNum];
+        if (!tagArray.includes(tag)) {
+            tagArray.push([tag, id]);
+        }  
+    } while (tagArray.length < 3)
+    return tagArray;
+}
+
+const displayTags = () => {
+    const root = document.getElementById('app');
+    root.innerHTML = '';
+    const form = document.createElement('form');
+    root.appendChild(form);
+    const allTags = getTags();
+    for (let tag of allTags) {
+        const selectDiv = document.createElement('div');
+        selectDiv.setAttribute('class', 'select');
+        form.appendChild(selectDiv);
+        const input = document.createElement('input');
+        input.setAttribute('type', 'radio');
+        input.setAttribute('id', `${tag[1]}`);
+        input.setAttribute('value', `${tag[0]}`);
+        input.setAttribute('name', 'displayTags')
+        input.addEventListener('click', function() { getArt(this.value, this.id) });
+        const label = document.createElement('label');
+        label.setAttribute('for', `${tag[1]}`)
+        label.innerHTML = tag[0];
+        selectDiv.appendChild(input);
+        selectDiv.appendChild(label);
+    }
+}
+
+const getArt = async (value, id) => {
+    console.log(value);
+    const idNum = id;
+    const endpoint = `${metUrl}/${idNum}`;
+    try {
+        const response = await fetch(endpoint, {cache: 'no-cache'});
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            // console.log(jsonResponse)
+            renderResponse(jsonResponse)
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const renderResponse = (val) => {
+    const background = document.getElementById('background');
+    // let imageLg = new Image();
+    // imageLg.src = val.primaryImage;
+    // // imageLg.setAttribute('class', hidden);
+    // imageLg.onload = () => {
+    //     console.log(imageLg.width, imageLg.height)
+    // }
+
+    // const root = document.getElementById('background');
+    // root.appendChild(imageLg);
+    
+
+    // console.log(val)
+    // console.log(val.measurements)
+    let imgDimensions;
+    if (val.measurements.length > 1) {
+        const imgFilter = val.measurements.filter(el => el.elementName == 'Overall')
+        imgDimensions = imgFilter[0]
+    } else {
+        imgDimensions = val.measurements[0] 
+    }
+    if(imgDimensions.elementMeasurements.Height > imgDimensions.elementMeasurements.width || !imgDimensions.elementMeasurements.width) {
+        background.classList.toggle('tall');
+    } else {background.classList.toggle('wide');}
+    // console.log(imgDimensions)
+
+
+
+    
+    
+    
+    background.style.backgroundImage = `url(${val.primaryImage})`;
+
+    // const root = document.getElementById('display');
+    // root.innerHTML = `<img src=${val.primaryImageSmall}>`
+    // // root.innerHTML = `<p>Title: ${val.title}</p><p>By: ${val.artistDisplayName}</p>`;
+}
